b10224d4defab9479ceac485b0e9b71a88db46ac
Jimmy G <neenreva@gmail.com>
2022-02-25 19:46:35 -0500

    Rework

--- app.js
+++ app.js
@@ -1,4 +1,7 @@
-document.querySelectorAll('.question-answer').forEach(ques => ques.addEventListener('click', (e) => {
-    e.target.classList.toggle('active');
-    e.target.parentNode.parentNode.children[1].classList.toggle('hidden');
-}))
+document
+  .querySelectorAll(".question-answer")
+  .forEach((el) => el.addEventListener("click", statusChange));
+
+function statusChange() {
+  this.children[1].classList.toggle("hidden");
+}
