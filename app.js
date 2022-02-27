62b1ea1bbd378481509442ccd1a4b9fc7cf4ade3
Jimmy G <neenreva@gmail.com>
2022-02-26 22:39:29 -0500

    Rework update

--- app.js
+++ app.js
@@ -3,5 +3,6 @@ document
   .forEach((el) => el.addEventListener("click", statusChange));
 
 function statusChange() {
+  this.children[0].children[0].classList.toggle("strong");
   this.children[1].classList.toggle("hidden");
 }
